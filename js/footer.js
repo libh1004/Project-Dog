b9aa1c2826cf260e38eb3daec60b29b967f04d86
Linh Nguyen <libh1004@gmail.com>
2020-11-24 08:41:59 +0700

    demo project

--- js/footer.js
+++ js/footer.js
@@ -1,52 +1,73 @@
 function footer() {
-    return "<div class=\"footer\">\n"+
-     "   <div class=\"links\">\n"+
-     "      <div class=\"row\">\n"+
-   " <div class=\"col-md-4\">\n"+
-     "              <i class=\"fas fa-paw\"></i>&nbsp&nbsp&nbsp<span>About Pet Shop</span>\n"+
-                    <p>Simply dummy text of the printing and typesetting industry. Lorem lpsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a gallery of type and scrambled it to make a type specimen book</p>
-                    <div class="icons">
-                        <i class="fab fa-facebook-f"></i>
-                        <i class="fab fa-twitter"></i>
-                        <i class="fab fa-google-plus-g"></i>
-                    </div>
+    return "<div class=\"links\">\n" +
+        "            <div class=\"row\">\n" +
+        "                <div class=\"col-md-4\">\n" +
+        "                    <div class=\"content\">\n" +
+        "                        <i class=\"fas fa-paw\"></i>&nbsp&nbsp&nbsp<span>About Pet Shop</span>\n" +
+        "                        <p class=\"item\">Simply dummy text of the printing and typesetting industry. Lorem lpsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a gallery of type and scrambled it to make a type specimen book</p>\n" +
+        "                        <div class=\"icons\">\n" +
+        "                            <i class=\"fab fa-facebook-f\" id=\"demo\" onclick=document.getElementById(\"demo\").innerHTML=\"ViewProfile\"></i>\n" +
+        "                            <i class=\"fab fa-twitter\"></i>\n" +
+        "                            <i class=\"fab fa-google-plus-g\"></i>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-2\">\n" +
+        "                    <div class=\"content\">\n" +
+        "                        <div class=\"icon\">\n" +
+        "                            <i class=\"fas fa-paw\"></i>\n" +
+        "                            &nbsp&nbsp&nbsp<span>Helpful Links</span>\n" +
+        "                        </div>\n" +
+        "                        <ul>\n" +
+        "                            <li class=\"item\"> Pet Dictionary</li>\n" +
+        "                            <li>Help Links</li>\n" +
+        "                            <li>Pet Attitudes</li>\n" +
+        "                            <li>Call a Doctor</li>\n" +
+        "                            <li>Help a Pet</li>\n" +
+        "                        </ul>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-2\">\n" +
+        "                    <div class=\"content\">\n" +
+        "                        <div class=\"icon\">\n" +
+        "                            <i class=\"fas fa-paw\"></i>\n" +
+        "                            &nbsp&nbsp&nbsp<span>Overview</span>\n" +
+        "                        </div>\n" +
+        "                            <ul>\n" +
+        "                                <li class=\"item\">Adopt a Pet</li>\n" +
+        "                                <li >Create Awareness</li>\n" +
+        "                                <li>Join Campaign</li>\n" +
+        "                            </ul>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-4\">\n" +
+        "                    <div class=\"content\">\n" +
+        "                            <i class=\"fas fa-paw\"></i>\n" +
+        "                            &nbsp&nbsp&nbsp<span>Subscribe</span>\n" +
+        "                        <input class=\"item\" id=\"prompt\" type=\"text\" name=\"...\"  placeholder=\"Your Email Address\"><br>\n" +
+        "                        <p id=\"note\">*&nbsp&nbsp We do not spam</p>\n" +
+        "                        <img src=\"imgs/footer_logo_img (1).png\" class=\"img1\"/>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "        <div class=\"finally\">\n" +
+        "            <div class=\"row\">\n" +
+        "                <div class=\"col-md-4\">\n" +
+        "                    <div class=\"text1\">\n" +
+        "                        <p>Copyright 2016 Pet Shop . All right reserved</p>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-6\"></div>\n" +
+        "                <div class=\"col-md-2\">\n" +
+        "                    <div class=\"text2\">\n" +
+        "                        <p>Terms &nbsp&nbsp&nbspPrivacy</p>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>"
 
-                </div>
-                <div class="col-md-2">
-                    <i class="fas fa-paw"></i>&nbsp&nbsp&nbsp<span>Helpful Links</span>
-                    <ul>
-                        <li> Pet Dictionary</li>
-                        <li>Help Links</li>
-                        <li>Pet Attitudes</li>
-                        <li>Call a Doctor</li>
-                        <li>Help a Pet</li>
-                    </ul>
-                </div>
-                <div class="col-md-2">
-                    <i class="fas fa-paw"></i>&nbsp&nbsp&nbsp<span>Overview</span>
-                    <ul>
-                        <li>Adopt a Pet</li>
-                        <li>Create Awareness</li>
-                        <li>Join Campaign</li>
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <i class="fas fa-paw"></i>&nbsp&nbsp&nbsp<span>Subscribe</span>
-                    <p>Your Email Address</p>
-                    <ul>
-                        <li> We do not spam</li>
-                    </ul>
-                    <img src="imgs/footer_logo_img (1).png" class="img1"/>
-                </div>
-            </div>
-        </div>
-        <div class="finally">
-            <ul>
-                <li>Copyright 2016 Pet Shop . All right reserved</li>
-                <li>Terms</li>
-                <li>Privacy</li>
-            </ul>
-        </div>
-    </div>
 
 }
+var ft = document.getElementById("footer");
+ft.innerHTML = footer();
--- js/header.js
+++ js/header.js
@@ -1,14 +1,134 @@
-function Top() {
-    return " <div class=\"top\">\n" +
-        "    <ul>\n" +
-        "        <li><a href=\"\"><img src=\"../imgs/home.png\" height=\"15\" width=\"15\"/>Home</a></li>\n" +
-        "        <li><a href=\"\"><img src=\"../imgs/icon1.png\" height=\"15\" width=\"15\"/>About</a></li>\n" +
-        "        <li><a href=\"\"><img src=\"../imgs/icon1.png\" height=\"15\" width=\"15\"/>News</a></li>\n" +
-        "        &emsp;&emsp;&emsp;&emsp;&emsp;<li><a href=\"\"><img src=\"../imgs/icon1.png\" height=\"15\" width=\"15\"/>Gallery</a></li>\n" +
-        "        <li><a href=\"\"><img src=\"../imgs/icon1.png\" height=\"15\" width=\"15\"/>Pages</a></li>\n" +
-        "        <li><a href=\"\"><img src=\"../imgs/icon1.png\" height=\"15\" width=\"15\"/>Contract</a></li>\n" +
+function header() {
+    return "<div class=\"header\">\n" +
+        "    <ul class=\"menu\">\n" +
+        "        <li class=\"item\" onmouseenter=\"menu(this)\" onmouseleave=\"closeMenu(this)\">\n" +
+        "            <i class=\"fas icon-nav fa-paw\"></i><a href=\"home_dog.html\" class=\"anchor\">Home</a>\n" +
+        "            <div class=\"submenu home\">\n" +
+        "                <p><a href=\"Home%20Classic.html\">Home Classic</a></p>\n" +
+        "                <p><a href=\"Home%20Services.html\">Home Services</a></p>\n" +
+        "            </div>\n" +
+        "        </li>\n" +
+        "        <li class=\"item\" onmouseenter=\"menu(this)\" onmouseleave=\"closeMenu(this)\">\n" +
+        "            <i class=\"fas icon-nav fa-paw\"></i><a href=\"About.html\" class=\"anchor\">About</a>\n" +
+        "            <div class=\"submenu about\">\n" +
+        "                <p><a href=\"Pet_Detail.html\">Pet Detail</a></p>\n" +
+        "                <a href=\"Sitter%20Detail.html\">Sitter Detail</a>\n" +
+        "            </div>\n" +
+        "        </li>\n" +
+        "        <li class=\"item\" onmouseenter=\"menu(this)\" onmouseleave=\"closeMenu(this)\">\n" +
+        "            <i class=\"fas icon-nav fa-paw\"></i><a href=\"New.html\" class=\"anchor\">News</a>\n" +
+        "            <div class=\"submenu news\">\n" +
+        "                <div class=\"row\">\n" +
+        "                    <div class=\"mega\">\n" +
+        "                        <div class=\"col-md-3\">\n" +
+        "                            <div class=\"useful\">\n" +
+        "                                <h4 class=\"change4\">Useful Links</h4>\n" +
+        "                                <ul>\n" +
+        "                                    <li>Adopt a Pet</li>\n" +
+        "                                    <li>Pet Care</li>\n" +
+        "                                    <li>Special Services</li>\n" +
+        "                                    <li>Pet Products</li>\n" +
+        "                                    <li>Pet Dictionary</li>\n" +
+        "                                    <li>Help Links</li>\n" +
+        "                                    <li>Pet Attitudes</li>\n" +
+        "                                    <li>Call a Doctor</li>\n" +
+        "                                    <li>Help a Pet</li>\n" +
+        "                                </ul>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"col-md-3\">\n" +
+        "                            <div class=\"recent\">\n" +
+        "                                <h4 class=\"change4\">Recent News</h4>\n" +
+        "                                <p>POSTED ON 16 AUGUST 2016</p>\n" +
+        "                                <p>Purus Integer at diam gravida </p>\n" +
+        "                                <p>POSTED ON 18 JUNE 2015</p>\n" +
+        "                                <p>Integer at diam gravida purus</p>\n" +
+        "                                <p>POSTED ON 18 JUNE 2015</p>\n" +
+        "                                <p>Praesent iaculis tortor viverra </p>\n" +
+        "                                <p>POSTED ON 18 JUNE 2015</p>\n" +
+        "                                <p>Aenean accumsan ligula diam </p>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"col-md-6\">\n" +
+        "                            <img src=\"imgs/megamenu_img.jpg\" class=\"image\"/>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </li>\n" +
+        "        <li class=\"item\">\n" +
+        "            <img class=\"logo\" src=\"imgs/logo@2x (1).png\" height=\"89\" width=\"72\"/></li>\n" +
+        "        <li class=\"item\" onmouseenter=\"menu(this)\" onmouseleave=\"closeMenu(this)\">\n" +
+        "            <i class=\"fas icon-nav fa-paw\"></i><a class=\"anchor\"></a>\n" +
+        "            <a href=\"Gallery.html\">Gallery</a>\n" +
+        "        </li>\n" +
+        "        <li class=\"item\" onmouseenter=\"menu(this)\" onmouseleave=\"closeMenu(this)\">\n" +
+        "            <i class=\"fas icon-nav fa-paw\"></i><a href=\"Pages.html\" class=\"anchor\">Pages</a>\n" +
+        "            <div class=\"submenu pages\">\n" +
+        "                <div class=\"row\">\n" +
+        "                    <div class=\"shortcodes\">\n" +
+        "                        <div class=\"col-md-3\">\n" +
+        "                            <div class=\"one\">\n" +
+        "                                <h4 class=\"change4\"><a href=\"home_dog.html\">Shortcodes I</a></h4>\n" +
+        "                                <p><a href=\"Blockquotes.html\">Blockquotes</a></p>\n" +
+        "                                <a href=\"Testimonials.html\">Testimonials</a>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"col-md-3\">\n" +
+        "                            <div class=\"two\">\n" +
+        "                                <h4 class=\"change4\"><a href=\"home_dog.html\">Shortcodes II</a></h4>\n" +
+        "                                <p><a href=\"Columns.html\">Columns</a></p>\n" +
+        "                                <a href=\"ImageWithCaption.html\">Image With Caption</a>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"col-md-3\">\n" +
+        "                            <div class=\"three\">\n" +
+        "                                <h4 class=\"change4\"><a href=\"home_dog.html\">Shortcodes III</a></h4>\n" +
+        "                                <p><a href=\"Carousel.html\">Carousel</a></p>\n" +
+        "                                <a href=\"fancy_boxes.html\">Fancy Boxes</a>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"col-md-3\">\n" +
+        "                            <div class=\"four\">\n" +
+        "                                <h4 class=\"change4\"><a href=\"home_dog.html\">Shortcodes IV</a></h4>\n" +
+        "                                <p><a href=\"Pages.html\">Typography</a></p>\n" +
+        "                                <a href=\"buttons & lists.html\">Buttons & Lists</a>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "                </div>\n" +
+        "        </li>\n" +
+        "        <li class=\"item\" onmouseenter=\"menu(this)\" onmouseleave=\"closeMenu(this)\">\n" +
+        "            <i class=\"fas icon-nav fa-paw\"></i><a class=\"anchor\"></a>\n" +
+        "            <a href=\"Contact.html\">Contact</a>\n" +
+        "        </li>\n" +
         "    </ul>\n" +
-        "</div>";
+        "</div>" +
+        "    <script type=\"text/javascript\">\n" +
+        "        function menu(a) {\n" +
+        "            var links = document.getElementsByClassName(\"item\");\n" +
+        "            for(var i=0;i<links.length;i++){\n" +
+        "                links[i].classList.remove(\"active\");\n" +
+        "            }\n" +
+        "            a.classList.add(\"active\");\n" +
+        "        }\n" +
+        "        function closeMenu(a) {\n" +
+        "            a.classList.remove(\"active\");\n" +
+        "        }\n" +
+        "    </script>"
+
 }
-var tp = document.getElementById("top");
-tp.innerHTML=Top();
+var hd = document.getElementById("header");
+hd.innerHTML=header();
+
+function menu(a) {
+    var links = document.getElementsByClassName('item');
+    for(var i=0;i<links.length;i++){
+        links[i].classList.remove('active');
+    }
+    a.classList.add('active');
+ }
+ function closeMenu(a) {
+    a.classList.remove('active');
+}
